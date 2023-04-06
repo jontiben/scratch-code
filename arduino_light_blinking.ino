edcd1b8f24656fd3f2704d94dc77c660213745e9
jontiben <jontiben@gmail.com>
2023-04-06 16:34:10 -0400

    Add files via upload

---
+++ arduino_light_blinking.ino
@@ -0,0 +1,34 @@
+const int INTER_BLINK_DELAY = 200;
+const int INTER_VALUE_DELAY = 1000;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  
+  // Fibonacci sequence as a flashing LED
+  int i = 0;
+  int j = 1;
+  int new_value;
+  int counter;
+  int internal_counter;
+
+  for (counter = 0; counter < 20; ++counter) {
+
+    for (internal_counter = 0; internal_counter < j; ++internal_counter) {
+      digitalWrite(13, HIGH);
+      delay(INTER_BLINK_DELAY);
+      digitalWrite(13, LOW);
+      delay(INTER_BLINK_DELAY);
+    }
+
+    new_value = i + j;
+    i = j;
+    j = new_value;
+
+    delay(INTER_VALUE_DELAY);
+
+  }
+}
